d09b94580595c0d5dc60d560f01ba268c5a2d38b
Dramane <Dramane.dev@gmail.com>
2022-06-25 13:15:21 +0200

    Fix error in url

--- simplytodo-api/src/classes/Main.class.ts
+++ simplytodo-api/src/classes/Main.class.ts
@@ -4,22 +4,17 @@ import compression from "compression";
 import { connectionToDatabase } from "../../db/connection.db";
 import { authRoutes } from "../routes/auth/auth.route";
 import { projectRoutes } from "../routes/project/project.route";
-import cors, { CorsOptions } from "cors";
+import cors from "cors";
 import { taskRoute } from "../routes/task/task.route";
 import { healthcheckRoutes } from "../routes/healthcheck/healthcheck";
 export default class Main {
     constructor(private _router: Express, private _port: number) {}
 
     initialization(): void {
-        const corsOptions: CorsOptions = {
-            origin: "http://localhost",
-            credentials: true
-        };
-
-        this._router.use(bodyParser.urlencoded({ extended: false }));
+        // this._router.use(bodyParser.urlencoded({ extended: false }));
         this._router.use(bodyParser.json());
         this._router.use(compression());
-        this._router.use(cors(corsOptions));
+        this._router.use(cors());
         this._router.use((req: Request, res: Response, next: NextFunction) => {
             res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
             next();
