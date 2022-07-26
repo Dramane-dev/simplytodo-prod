e7cf903642bc9ee58811191121481f8e5badd395
Dramane <Dramane.dev@gmail.com>
2022-07-26 18:49:40 +0200

    simplytodo-api bug fixed

--- simplytodo-api/src/classes/Main.class.ts
+++ simplytodo-api/src/classes/Main.class.ts
@@ -19,14 +19,18 @@ export default class Main {
             res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
             next();
         });
-        this._router.use("/simplytodo/api", healthcheckRoutes);
-        this._router.use("/simplytodo/api", authRoutes);
-        this._router.use("/simplytodo/api", projectRoutes);
-        this._router.use("/simplytodo/api", taskRoute);
-        // this._router.use("/", healthcheckRoutes);
-        // this._router.use("/", authRoutes);
-        // this._router.use("/", projectRoutes);
-        // this._router.use("/", taskRoute);
+
+        // To prod
+        // this._router.use("/simplytodo/api", healthcheckRoutes);
+        // this._router.use("/simplytodo/api", authRoutes);
+        // this._router.use("/simplytodo/api", projectRoutes);
+        // this._router.use("/simplytodo/api", taskRoute);
+
+        // To local
+        this._router.use("/api", healthcheckRoutes);
+        this._router.use("/api", authRoutes);
+        this._router.use("/api", projectRoutes);
+        this._router.use("/api", taskRoute);
     }
 
     startServer(): void {
