2227ec918ae3af98e3db45154fb6e2e5c53e16b8
Dramane <Dramane.dev@gmail.com>
2022-09-09 21:17:58 +0200

    Local version

--- simplytodo-api/src/classes/Main.class.ts
+++ simplytodo-api/src/classes/Main.class.ts
@@ -20,17 +20,10 @@ export default class Main {
             next();
         });
 
-        // To prod
-        this._router.use("/simplytodo/api", healthcheckRoutes);
-        this._router.use("/simplytodo/api", authRoutes);
-        this._router.use("/simplytodo/api", projectRoutes);
-        this._router.use("/simplytodo/api", taskRoute);
-
-        // To local
-        // this._router.use("/api", healthcheckRoutes);
-        // this._router.use("/api", authRoutes);
-        // this._router.use("/api", projectRoutes);
-        // this._router.use("/api", taskRoute);
+        this._router.use("/api", healthcheckRoutes);
+        this._router.use("/api", authRoutes);
+        this._router.use("/api", projectRoutes);
+        this._router.use("/api", taskRoute);
     }
 
     startServer(): void {
