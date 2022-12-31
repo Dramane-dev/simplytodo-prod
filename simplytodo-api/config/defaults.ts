e1ba4cd9b0872ece2cf9188453e1a2889573efd7
Dramane <Dramane.dev@gmail.com>
2022-12-31 19:28:34 +0100

    update defaults config file

--- simplytodo-api/config/defaults.ts
+++ simplytodo-api/config/defaults.ts
@@ -3,10 +3,15 @@ dotenv.config();
 
 export default {
     port: parseInt(String(process.env.PORT)),
-    mySqlHost: process.env.MY_SQL_HOST,
+    mySqlHost: process.env.NODE_ENV !== 'production'
+    ? process.env.MY_SQL_LOCALHOST
+    : process.env.MY_SQL_HOST,
     mySqlDatabaseName: String(process.env.MY_SQL_DATABASE),
     mysqlPort: parseInt(String(process.env.MY_SQL_PORT)),
-    mySqlUser: String(process.env.MY_SQL_USER),
+    mySqlUser: 
+    process.env.NODE_ENV !== 'production'
+    ? String(process.env.MY_SQL_LOCAL_USER)
+    : String(process.env.MY_SQL_USER),
     mySqlPassword: String(process.env.MY_SQL_PASSWORD),
     refreshTokens: [] as string[],
 };
